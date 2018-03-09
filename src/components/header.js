5615948c86c0982d9fc2da5cfbe065a5444ad687
JordiOrtega <jordi.ortega@gmail.com>
2018-03-09 14:28:09 +0100

    Coffee header.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const header = () => {
         <header className="App-header">
             <div className="row">
                 <div className="col text-center">
-                    <h1 className="App-title">React Coffe Vending</h1>
+                    <h1 className="App-title">React Coffee Vending</h1>
                 </div>
             </div>
         </header>
